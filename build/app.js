d4536b5baa09b386171b2cab83f6f1e01ac933e7
Nogyboy <guamanc9@gmail.com>
2023-04-25 17:35:09 -0500

    Delete build

--- build/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-require("./config/config");
-const config_1 = require("./server/config");
-const server = new config_1.Server();
-server.listen();
