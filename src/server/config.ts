1b21f712249465f37ffa3089488504784aabf9b0
Nogyboy <guamanc9@gmail.com>
2023-04-25 16:43:06 -0500

    Fixes

--- src/server/config.ts
+++ src/server/config.ts
@@ -18,7 +18,7 @@ export class Server {
 
   constructor() {
     this.app = express();
-    this.port = process.env.PORT || "3000";
+    this.port = process.env.PORT!;
     this.paths = {
       testServer: "/",
       user: "/user",
